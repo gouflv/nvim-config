bb63ab65916ead58d47b57c6ebcf0231d16d9e0b
gouflv <lv.gouf@gmail.com>
2022-08-21 01:55:16 +0800

    change keymaps

--- lua/maps.lua
+++ lua/maps.lua
@@ -5,6 +5,9 @@ vim.g.mapleader = ' '
 map('i', 'jj', '<Esc>')
 map('i', 'jk', '<Esc>')
 
+-- Change text without putting into register
+map('n', 'c', '"_c')
+
 -- Replace selection with text in reg
 map('v', 'p', '"_dP')
 
@@ -19,22 +22,26 @@ map('v', '>', '>gv')
 map('v', '<', '<gv')
 
 -- Keep cursor position after yanking
-map('v', 'y', 'myy`y')
+map('v', 'y', 'myy')
 
 -- Add empty line
 map('n', '<leader>o', 'o<Esc>')
 map('n', '<leader>O', 'O<Esc>')
 
+-- Save file
+map('n', '<leader>s', ':w<CR>')
+
 -- Split window
 map('n', '<leader>w', '<C-w>')
+map('n', '<leader>ww', '<C-w>c')
 map('n', '<leader>h', '<C-w>h')
 map('n', '<leader>j', '<C-w>j')
 map('n', '<leader>k', '<C-w>k')
 map('n', '<leader>l', '<C-w>l')
 
 -- Buffer
-map('n', 'gn', ':bnext<CR>')
-map('n', 'gp', ':bprevious<CR>')
+map('n', '<C-n>', ':bnext<CR>')
+map('n', '<C-p>', ':bprevious<CR>')
 map('n', '<leader>c', ':Bdelete<CR>')
 
 -- Run Command easier
