b69e5513faee00cba9210736efe8371a833f8a36
gouflv <lv.gouf@gmail.com>
2022-08-25 17:14:52 +0800

    migrate bufferline config

--- lua/configs/bufferline.lua
+++ lua/configs/bufferline.lua
@@ -2,7 +2,7 @@ local status, bufferline = pcall(require, 'bufferline')
 if not status then return end
 
 local getcwd = function(path)
-  local reg = "([^/]+)$"
+  local reg = '([^/]+)$'
   return string.match(path, reg) or path
 end
 
@@ -19,6 +19,8 @@ bufferline.setup({
     show_buffer_close_icons = false,
     show_close_icon = false,
     show_tab_indicators = false,
-    indicator_icon = '',
+    indicator = {
+      style = 'none'
+    }
   }
 })
