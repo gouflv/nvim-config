27f0f4e90bc0fe2e06b99cbc709ed002b98fcd8b
gouflv <lv.gouf@gmail.com>
2022-09-07 14:06:51 +0800

    update base options

--- lua/base.lua
+++ lua/base.lua
@@ -1,12 +1,14 @@
 local vim = vim
 
+-- aaabadfds
+
 vim.opt.fileencoding = 'utf-8'
 
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
-vim.opt.wrap = false
+-- vim.opt.wrap = false
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
@@ -14,13 +16,16 @@ vim.opt.scrolloff = 10
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.opt.iskeyword:append('-')
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
+
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.swapfile = false
 vim.opt.clipboard:append({ 'unnamedplus' })
 vim.opt.mouse = 'a'
+vim.opt.wildmode = 'list:longest,full'
 
 -- Highlight Yanked Text
 vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
