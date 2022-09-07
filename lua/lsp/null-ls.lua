df098f300b112f3093932ce0ac96209ad6852f93
gouflv <lv.gouf@gmail.com>
2022-09-07 11:59:28 +0800

    formatting on save

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -5,8 +5,10 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+
 null_ls.setup({
-  debug = true,
+  debug = false,
   sources = {
     -- ESLint
     diagnostics.eslint_d,
@@ -17,4 +19,16 @@ null_ls.setup({
 
     --diagnostics.cspell,
   },
+  on_attach = function(client, bufnr)
+    if client.supports_method('textDocument/formatting') then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.formatting_sync()
+        end
+      })
+    end
+  end
 })
