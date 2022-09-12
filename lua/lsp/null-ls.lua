4377e88540b86f530972bf3088771e92a4cb12f1
gouflv <lv.gouf@gmail.com>
2022-09-12 18:00:14 +0800

    add eslint running condition

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -7,12 +7,28 @@ local code_actions = null_ls.builtins.code_actions
 
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
+local eslint_condition = function(utils)
+  return utils.root_has_file({
+    '.eslintrc',
+    '.eslintrc.js',
+    '.eslintrc.json',
+  })
+
+end
+
 null_ls.setup({
   debug = false,
   sources = {
     -- ESLint
-    diagnostics.eslint_d,
-    code_actions.eslint_d,
+    diagnostics.eslint_d.with({
+      -- filter = function (diagnostics)
+      --   return diagnostics.code ~= 'prettier/prettier'
+      -- end,
+      condition = eslint_condition
+    }),
+    code_actions.eslint_d.with({
+      condition = eslint_condition
+    }),
 
     -- Prettier
     formatting.prettier,
