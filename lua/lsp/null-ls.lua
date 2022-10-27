d33c108fc4d6c679958e84fb43462fa645b68412
gouflv <lv.gouf@gmail.com>
2022-10-27 14:02:19 +0800

    adapt nvim0.8 api changes

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -60,7 +60,7 @@ null_ls.setup({
       vim.api.nvim_create_autocmd('BufWritePre', {
         group = augroup,
         buffer = bufnr,
-        callback = function() vim.lsp.buf.formatting_sync() end
+        callback = function() vim.lsp.buf.format() end
       })
     end
 
