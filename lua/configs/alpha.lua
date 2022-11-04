7fd319b91dd29c5c9acf28ec582d606c422d5f79
gouflv <lv.gouf@gmail.com>
2022-11-05 01:55:13 +0800

    update nvim-tree keymap

--- lua/configs/alpha.lua
+++ lua/configs/alpha.lua
@@ -3,9 +3,8 @@ if not status then return end
 
 local dashboard = require('alpha.themes.dashboard')
 
-
 dashboard.section.buttons.val = {
-  dashboard.button('po', 'Explorer', ':NvimTreeOpen <CR>'),
+  dashboard.button('ee', 'Explorer', ':NvimTreeOpen <CR>'),
   dashboard.button('n', 'New file', ':ene <BAR> startinsert <CR>'),
   dashboard.button('ff', 'Find file', ':Telescope find_files <CR>'),
   dashboard.button('fo', 'Recent', ':Telescope oldfiles <CR>'),
--- lua/configs/nvim-tree.lua
+++ lua/configs/nvim-tree.lua
@@ -22,7 +22,7 @@ nvim_tree.setup({
       show = {
         folder = false,
         file = false,
-        git = false
+        -- git = false
       }
     }
   },
@@ -39,5 +39,5 @@ nvim_tree.setup({
 --    autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
 --]])
 
-map('n', '<leader>pf', '<cmd>lua require("nvim-tree").toggle()<CR>')
-map('n', '<leader>pg', '<cmd>lua require("nvim-tree").find_file()<CR>')
+map('n', '<leader>ee', '<cmd>lua require("nvim-tree").toggle()<CR>')
+map('n', '<leader>ef', '<cmd>lua require("nvim-tree").toggle({ find_file = true })<CR>')
