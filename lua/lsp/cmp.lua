8c8cf3f5e916136abcf2557c501555448ee03d0b
gouflv <lv.gouf@gmail.com>
2022-11-18 19:25:47 +0800

    cmp keymap

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -4,27 +4,25 @@ if not cmp_status then return end
 local snip_status, luasnip = pcall(require, 'luasnip')
 if not snip_status then return end
 
-vim.opt.completeopt = 'menuone,noinsert,noselect'
-
 cmp.setup({
   snippet = {
     expand = function(args) luasnip.lsp_expand(args.body) end
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'buffer' },
     { name = 'path' },
+    { name = 'buffer', keyword_length = 2 },
   }),
   mapping = cmp.mapping.preset.insert({
     -- Select
-    ['<CR>'] = cmp.mapping.confirm({
-      select = false,
-      behavior = cmp.ConfirmBehavior.Insert
-    }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
 
     -- Navigate
     ['<Up>'] = cmp.mapping.select_prev_item(),
     ['<Down>'] = cmp.mapping.select_next_item(),
+
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
     -- Doc
     ['K'] = cmp.mapping.scroll_docs(-5),
@@ -34,38 +32,11 @@ cmp.setup({
     ['<C-e>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.close()
-        fallback()
       else
         cmp.complete()
       end
     end),
-
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        local entry = cmp.get_selected_entry()
-        if not entry then
-          cmp.select_next_item()
-        else
-          cmp.confirm()
-        end
-      else
-        fallback()
-      end
-    end, {
-      'i',
-      's',
-    }),
-
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end, {
-      'i',
-      's',
-    }),
+    ['<Esc>'] = cmp.mapping.close(),
   }),
   formatting = {
     format = require('lspkind').cmp_format({
